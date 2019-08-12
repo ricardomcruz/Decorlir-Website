7f23bed147e89df05ed40d01ee036ad87d52c207
Ricardo Cruz <ricardo.mj.cruz@gmail.com>
2019-08-12 16:47:46 +0100

    monday

---
+++ form.js
@@ -0,0 +1,10 @@
+function switchContact(){
+    var x = document.getElementById("form");
+    var y = document.getElementById("map");
+    x.classList.toggle("js_show");
+    y.classList.toggle("js_show");
+    x.classList.toggle("js_hide");
+    y.classList.toggle("js_hide");
+}
+
+
