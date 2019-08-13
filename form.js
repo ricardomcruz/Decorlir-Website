fc0af7d9e349462f1bb70c6900415697ab6d4c19
Ricardo Cruz <ricardo.mj.cruz@gmail.com>
2019-08-13 15:11:15 +0100

    go

--- form.js
+++ form.js
@@ -1,11 +1,24 @@
 var x = document.querySelector("form");
 var y = document.getElementById("gmap_canvas");
 
+var btn1 = document.getElementById("btn_map");
+var btn2 = document.getElementById("btn_form");
+
 
 function toggleForm(){
     
     
         x.classList.toggle("form_show");
+
+        y.classList.toggle("form_show");
+        if (btn2.innerHTML == "Pedir Orçamento Gratuito") {
+            btn2.innerHTML = "Ocultar Formulário";
+          } else {
+            btn2.innerHTML = "Pedir Orçamento Gratuito";
+          }
+    
+
+        
    
 }
 
@@ -15,5 +28,10 @@ function toggleMap(){
     
     
     y.classList.toggle("form_show");
+    if (btn.innerHTML == "ver mapa") {
+        btn.innerHTML = "ocultar mapa";
+      } else {
+        btn.innerHTML = "ver mapa";
+      }
 
 }
